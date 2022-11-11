e8dc40852f86e7aa822b4611b876a1b4e0e2dbbd
NguyenTanDuoc <tan-duoc@system-exe.com.vn>
2022-11-11 12:43:23 +0700

    delete file

--- README.md
+++
@@ -1,4 +0,0 @@
-# JUnit with TestContainers
-https://www.testcontainers.org/
-# Install Docker
-https://docs.docker.com/desktop/install/windows-install/
--- src/main/resources/import_employees.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO employee (employee_id,full_name,email,position,birth_day,phone,department) VALUES
-	 (283,'Le Minh Nghia','minh-nghia@system-exe.com.vn','Developer','2001-10-02','0348400961','Offshore');
