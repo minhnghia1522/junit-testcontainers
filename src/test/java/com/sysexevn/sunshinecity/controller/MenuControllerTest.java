b741bcd8b9c3ca095956132187a8d21051d4c6cb
TranHuyPhuc <huy-phuc@system-exe.local>
2022-11-15 09:31:48 +0700

    test-menu-controller

--- src/test/java/com/sysexevn/sunshinecity/controller/MenuControllerTest.java
+++ src/test/java/com/sysexevn/sunshinecity/controller/MenuControllerTest.java
@@ -3,6 +3,7 @@ package com.sysexevn.sunshinecity.controller;
 import static com.sysexevn.sunshinecity.utils.CommonUtils.asJsonString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -21,6 +22,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import com.sysexevn.sunshinecity.config.AbsTest;
 import com.sysexevn.sunshinecity.dto.MenuDto;
@@ -30,32 +32,33 @@ public class MenuControllerTest extends AbsTest {
 
 	@Autowired
 	private MockMvc mockMvc;
-	
+
 	@DisplayName("Test-Create-Menu")
 	@Test
 	@Order(1)
 	public void createMenu() throws Exception {
 		MenuDto dto = new MenuDto();
 		dto.setMenuName("co111");
-		this.mockMvc.perform(post("/menu/addmenu")
-				.accept(MediaType.APPLICATION_JSON)//
+		MvcResult result = this.mockMvc.perform(post("/menu/addmenu").accept(MediaType.APPLICATION_JSON)//
 				.contentType(MediaType.APPLICATION_JSON_VALUE)//
 				.content(asJsonString(dto)))//
 				.andDo(print())//
-				.andExpect(status().isOk());
+				.andExpect(status().isOk()).andReturn();
+		String resultDOW = result.getResponse().getContentAsString();
+		assertNotNull(resultDOW.contains("success"));
 	}
-	
+
 	@DisplayName("Test-Get-Menu-By-ID")
 	@Test
 	@Order(2)
 	public void getMenuById() throws Exception {
 		MenuDto dto = new MenuDto();
 		dto.setMenuName("co111");
-		this.mockMvc.perform(get("/menu/" + 1)
-				.content(asJsonString(dto)))
-				.andDo(print())
+		MvcResult result = this.mockMvc.perform(get("/menu/" + 1).content(asJsonString(dto))).andDo(print())
 				.andExpect(status().isOk())//
-				.andExpect(jsonPath("$.menuName", is(dto.getMenuName())));
+				.andExpect(jsonPath("$.menuName", is(dto.getMenuName()))).andReturn();
+		String resultDOW = result.getResponse().getContentAsString();
+		assertNotNull(resultDOW.contains("success"));
 	}
 
 	@DisplayName("Test-Get-All-Menu")
@@ -64,13 +67,13 @@ public class MenuControllerTest extends AbsTest {
 	public void getAllMenu() throws Exception {
 		MenuDto dto = new MenuDto();
 		dto.setMenuName("co111");
-		this.mockMvc.perform(get("/menu/getAll")
-				.content(asJsonString(dto)))
-				.andDo(print())
-				.andExpect(status().isOk());//
-//				.andExpect(jsonPath("$.menuName", is(dto.getMenuName())));
+		MvcResult result = this.mockMvc.perform(get("/menu/getAll").content(asJsonString(dto))).andDo(print())
+				.andExpect(status().isOk()).andReturn();
+		String resultDOW = result.getResponse().getContentAsString();
+		assertNotNull(resultDOW.contains("success"));
+
 	}
-	
+
 	@DisplayName("Test-Update-Menu")
 	@Test
 	@Order(4)
@@ -79,13 +82,15 @@ public class MenuControllerTest extends AbsTest {
 		dto.setMenuId(1);
 		dto.setMenuName("co111");
 		dto.setUpdateAt(new Date());
-		this.mockMvc.perform(put("/menu/updatemenu")
-				.accept(MediaType.APPLICATION_JSON)//
+		MvcResult result = this.mockMvc.perform(put("/menu/updatemenu").accept(MediaType.APPLICATION_JSON)//
 				.contentType(MediaType.APPLICATION_JSON_VALUE)//
 				.content(asJsonString(dto)))//
 				.andDo(print())//
-				.andExpect(status().isOk());
+				.andExpect(status().isOk()).andReturn();
+		String resultDOW = result.getResponse().getContentAsString();
+		assertNotNull(resultDOW.contains("success"));
 	}
+
 	@DisplayName("Test-Delete-Menu")
 	@Test
 	@Order(5)
@@ -94,18 +99,17 @@ public class MenuControllerTest extends AbsTest {
 		dto.setMenuId(1);
 		dto.setMenuName("co111");
 		dto.setUpdateAt(new Date());
-		this.mockMvc.perform(delete("/menu/delete")
-				.accept(MediaType.APPLICATION_JSON)//
+		MvcResult result = this.mockMvc.perform(delete("/menu/delete").accept(MediaType.APPLICATION_JSON)//
 				.contentType(MediaType.APPLICATION_JSON_VALUE)//
 				.content(asJsonString(dto)))//
 				.andDo(print())//
-				.andExpect(status().isOk());
+				.andExpect(status().isOk()).andReturn();
+		String resultDOW = result.getResponse().getContentAsString();
+		assertNotNull(resultDOW.contains("success"));
 	}
 
 	public void shouldReturnDefaultMessage() throws Exception {
-		this.mockMvc.perform(get("/menu/"))
-				.andDo(print())
-				.andExpect(status().isOk())
+		this.mockMvc.perform(get("/menu/")).andDo(print()).andExpect(status().isOk())
 				.andExpect(content().string(containsString("Hello, World")));
 	}
 }
